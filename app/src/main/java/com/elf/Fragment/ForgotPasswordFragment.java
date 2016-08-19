ddc6635cb373c7eea921271afa27ef8424c0916e
nanborklabs <nandhu12195@gmail.com>
2016-08-19 16:44:28 +0530

    forgot password

--- app/src/main/java/com/elf/Fragment/ForgotPasswordFragment.java
+++ app/src/main/java/com/elf/Fragment/ForgotPasswordFragment.java
@@ -2,12 +2,26 @@ package com.elf.Fragment;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.TextInputEditText;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.elf.Network.CustomRetryPolicy;
+import com.elf.Network.ElfRequestQueue;
 import com.elf.R;
+
+import org.json.JSONObject;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 /**
  * Created by nandhu on 9/8/16.
@@ -16,7 +30,23 @@ public class ForgotPasswordFragment extends Fragment {
 
 
 
+    private static final String TAG = "FORGOT";
+    private static final String CHECK_URL = "";
+    private static final String SUBMIT_URL = " ";
+
+
+    @BindView(R.id.fp_email_ted)
+    TextInputEditText mEamilBox;
+    @BindView(R.id.fp_phone_ted) TextInputEditText mPhoneBox;
+    @BindView(R.id.fp_check_button)
+    Button mCheckButton;
     private View mView;
+    @BindView(R.id.fp_pass_ted) TextInputEditText mPasswordBox;
+    @BindView(R.id.fp_rpass_ted) TextInputEditText mRePassBox;
+    @BindView(R.id.fp_new_password_button) Button mSubmitButton;
+
+    ElfRequestQueue mRequestQue;
+
     @Override
     public void onDestroyView() {
         super.onDestroyView();
@@ -30,13 +60,59 @@ public class ForgotPasswordFragment extends Fragment {
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        mRequestQue = ElfRequestQueue.getInstance(getContext());
+
+
+
+
+
     }
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
      mView=inflater.inflate(R.layout.forgot_password,container,false);
+        ButterKnife.bind(this,mView);
+        mView.findViewById(R.id.forgot_password).setVisibility(View.VISIBLE);
+        mCheckButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final String mEmail = mEamilBox.getText().toString();
+                final String mPhone = mPhoneBox.getText().toString();
+
+                //validate user name and password for this user
+                validateUser(mEmail,mPhone);
+            }
+        });
+
         return mView;
+    }
+
+    private void validateUser(String mEmail, String mPhone) {
+        //make objects get Repsonse , if success show reset password page
+        final JSONObject mObject = new JSONObject();
+        try {
+
+            mObject.put("Email", mEmail);
+            mObject.put("phone", mPhone);
+        } catch (Exception e) {
+            Log.d(TAG, "validateUser: ");
+        }
+
+        JsonObjectRequest mRequest = new JsonObjectRequest(Request.Method.POST, CHECK_URL, mObject, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                Log.d(TAG, "onResponse: " + response.toString());
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.d(TAG, "onErrorResponse: " + error.getLocalizedMessage());
+            }
+        });
+
+        mRequest.setRetryPolicy(new CustomRetryPolicy());
+        mRequestQue.addToElfREquestQue(mRequest);
     }
 
     @Override
